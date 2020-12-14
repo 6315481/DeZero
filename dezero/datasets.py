9fd3f300a82ca76dc588f96525b1614248774663
unknown <Sho Okumoto>
2020-12-15 02:02:39 +0900

    add datasets module

---
+++ dezero/datasets.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+class Dataset:
+
+    def __init__(self, train=True):
+        self.train = train
+        self.data = None
+        self.label = None
+        self.prepare()
+
+    def __getitem__(self, index):
+        assert np.isscalar(index)
+
+        if self.label is None:
+            return self.data[index], None
+        else:
+            return self.data[index], self.label[index]
+
+    
+    def __len__(self):
+        return len(self.data)
+    
+    def prepare(self):
+        pass
+
